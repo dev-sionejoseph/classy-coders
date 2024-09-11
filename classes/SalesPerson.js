a452a5b809d23022b55c4280c61cb6aa472605a6
Sione <dev.sionejoseph@gmail.com>
2024-09-11 13:44:19 -0500

    Sales Person class passing all tests

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,5 +1,21 @@
-class SalesPerson {
+const { Employee } = require("./Employee");
 
+class SalesPerson extends Employee {
+    #totalSales;
+
+    constructor(name, title, salary, clients){
+        super(name, title, salary);
+        this.#totalSales = 0;
+        this.clients = clients;
+    }
+
+    getSalesNumber(){
+        return this.#totalSales;
+    }
+
+    makeSale(amount){
+        this.#totalSales += amount;
+    }
 
 
     
