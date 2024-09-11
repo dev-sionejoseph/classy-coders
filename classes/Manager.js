634225365f6912c49c82f95f53eb54a3e1e3bb1b
Sione <dev.sionejoseph@gmail.com>
2024-09-11 13:22:51 -0500

    Manager class passing all tests

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,7 +1,22 @@
-class Manager {
+const { Employee } = require("./Employee");
 
+class Manager extends Employee {
+    #employeesManaged;
 
+    constructor(name, title, salary, department, numEmployees){
+        super(name, title, salary);
+        this.department = department;
+        this.numEmployees = numEmployees;
+        this.#employeesManaged = [];
+    }
 
+    getEmployeesManaged(){
+        return this.#employeesManaged;
+    }
+    
+    setEmployeesManaged(employee){
+        this.#employeesManaged.push(employee);
+    }
     
 }
 
